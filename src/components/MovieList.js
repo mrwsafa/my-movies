5dd0a336eee884e17c222ef008a89c0beb2bd9af
mrwsafa <58708072+mrwsafa@users.noreply.github.com>
2021-04-10 20:23:00 +0300

    lab app bootsrap added

---
+++ src/components/MovieList.js
@@ -0,0 +1,29 @@
+
+import React, { Component } from 'react';
+
+export default class MovieList extends Component {
+    render() {
+        return (
+            <div className="row">
+                {this.props.movies.map((movie) =>)}
+                <div className="col-lg-4">
+                    <div className= "card mb-4 shadow-sm">
+                        <img src= "https://image.tmdb.org/t/p/w220_and_h330_face/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg" className="card-img-top" alt="Sample Movie"/>
+                        <div className="card-body">
+                            <h5 className="card-title"> Sample Movie</h5>
+                            <p className="card-text">Sample Movie Description</p>
+                            <div className="d-flex justify-content-between align-items-center">
+                                <button type="button" className="btn btn-md btn-outline-danger">Delete</button>
+                                <h2><span className="badge badge-info">9.0</span></h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                )}; 
+            </div>
+            
+        )
+    }
+}
+
+    
---
+++ src/components/SearchBar.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+    render() {
+        return (
+            <div>
+             <h1>SearchBar</h1> 
+            </div>
+        )
+    }
+}
+
+
+    
